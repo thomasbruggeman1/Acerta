fa6cd0e13bc1ce155c7eb859530bd40aaa21f7c0
thomas.bruggeman <thomas.bruggeman@student.howest.be>
2021-12-02 12:07:12 +0100

    css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,48 +4,58 @@
 @section("main")
     <h2>Fill in the form</h2>
 
-    @foreach ($records as $record)
+    <!-- @foreach ($records as $record)
         <option value="{{$record -> Projectname}}">{{$record -> Projectname}}</option>
-    @endforeach
+    @endforeach -->
 
 
 
 
-    <form method="post" action="{{route('process')}}">
+    <form class="form-style-4" method="post" action="{{route('process')}}">
         @csrf
-        <label for="Budgetcode">Budgetcode:</label>
-        <input type="text" id="Budgetcode" name="Budgetcode">
+        <label for="Budgetcode">
+        <span>Budgetcode:</span><input type="text" id="Budgetcode" name="Budgetcode">
+        </label>
 
-        <label for="Domein">Domein:</label>
-        <input type="text" id="Domein" name="Domein">
+        <label for="Domein">
+        <span>Domein:</span><input type="text" id="Domein" name="Domein">
+        </label>
 
-        <label for="Epicnr">Epicnr:</label>
-        <input type="text" id="Epicnr" name="Epicnr">
+        <label for="Epicnr">
+        <span>Epicnr:</span><input type="text" id="Epicnr" name="Epicnr">
+        </label>
 
-        <label for="Projectname">Projectname:</label>
-        <input type="text" id="Projectname" name="Projectname">
+        <label for="Projectname">
+        <span>Projectname:</span><input type="text" id="Projectname" name="Projectname">
+        </label>
 
-        <label for="Budget_owner_business">Budget_owner_business:</label>
-        <input type="text" id="Budget_owner_business" name="Budget_owner_business">
+        <label for="Budget_owner_business">
+        <span>Budget_owner_business:</span><input type="text" id="Budget_owner_business" name="Budget_owner_business">
+        </label>
 
-        <label for="Budget_owner_ict">Budget_owner_ict:</label>
-        <input type="text" id="Budget_owner_ict" name="Budget_owner_ict">        
-    
-        <label for="Baseline_budget">Baseline_budget:</label>
-        <input type="number" id="Baseline_budget" name="Baseline_budget">
-        
-        <label for="Baseline_facturen">Baseline_budget:</label>
-        <input type="number" id="Baseline_facturen" name="Baseline_facturen">
+        <label for="Budget_owner_ict">
+        <span>Budget_owner_ict:</span><input type="text" id="Budget_owner_ict" name="Budget_owner_ict">        
+        </label>
 
-        <label for="Start">Start:</label>
+        <label for="Baseline_budget">
+        <span>Baseline_budget:</span><input type="number" id="Baseline_budget" name="Baseline_budget">
+        </label>
+
+        <label for="Baseline_facturen">
+        <span>Baseline_facturen:</span><input type="number" id="Baseline_facturen" name="Baseline_facturen">
+        </label>
+
+        <label for="Start">Start:
         <input type="date" id="Start" name="Start">
+        </label>
 
-        <label for="Finish">Finish:</label>
+        <label for="Finish">Finish:
         <input type="date" id="Finish" name="Finish">
+        </label>
 
 
         <label for="Comments">Comments:</label>
-        <input type="text" id="Comments" name="Comments">
+        <input type="textarea" onkeyup="adjust_textarea(this)"  id="Comments" name="Comments">
 
         <input type="submit" value="Add">
 
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -2,14 +2,25 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="{{ asset('/assets/css/reset.css') }}">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('/assets/css/screen.css') }}">
+    <title>Acerta</title>
 </head>
 <body>
+    <!-- <div class="svg-container">
+    <svg viewbox="0 0 800 400" class="svg">
+      <path id="curve" fill="#50c6d8" d="M 800 300 Q 400 350 0 300 L 0 0 L 800 0 L 800 300 Z">
+      </path>
+    </svg>
+    </div>
     <header>
-        <h1>Acerta</h1>
-    </header>
+        <div><figure>
+        <img src="{{ asset('/media/Acerta.png') }}" alt="Acerta"
+                 title="Acerta"/>
+        </figure></div>
+    </header> -->
 
     <main>
         @yield("main")
@@ -17,7 +28,7 @@
 
     <footer>
         <hr/>
-        created by AcertaTeam
+       © created by AcertaTeam
     </footer>
 </body>
 </html>
